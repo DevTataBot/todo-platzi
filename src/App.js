73ed10401697e2bf9c9225634619cccecc12af02
Tatiana Botia <tatianabb21@gmail.com>
2023-01-24 22:59:46 -0500

    styles and events

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateTodoButton from "./Components/CreateTodo";
 import TodoCounter from "./Components/TodoCounter";
 import TodoList from "./Components/TodoList";
 import TodoSearch from "./Components/TodoSearch";
 
 const App = () => {
+  const [valueSearch, setValueSearch] = useState('')
+
   const todos = [
     { text: "Tarea tuturutu", complete: false },
-    { text: "Tarea tuturut2", complete: false },
+    { text: "Tarea tuturut2", complete: true },
     { text: "Estudiar", complete: false },
   ];
   return (
     <>
       <TodoCounter />
-      <TodoSearch />
+      <TodoSearch valueSearch={valueSearch} setValueSearch={setValueSearch} />
       <TodoList list={todos} />
+      <CreateTodoButton />
     </>
   );
 };
--- src/Components/TodoItems/index.js
+++ src/Components/TodoItems/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./styles.css";
 const TodoItems = ({ task }) => {
+    const handlerCheckTask = () => {
+        console.log('tarea hecha')
+    }
+    const handlerDeleteTask = () => {
+        console.log('tarea eliminada')
+    }
   return (
-    <div className="TodoItem">
-      <input className="TodoItem__checkbox" type="checkbox" />
-      <div className="TodoItem__container">
-        <li className="TodoItem__item">{task.text}</li>
-        <button className="TodoItem__delete">X</button>
+    <li className="TodoItem">
+      <input className="TodoItem__checkbox" type="checkbox" onChange={handlerCheckTask} />
+      <div className={`TodoItem__container ${task.complete && 'complete'}`}>
+        <span className={`TodoItem__item ${task.complete && 'complete__text'}`}>{task.text}</span>
+        <button className="TodoItem__delete" onClick={handlerDeleteTask}>X</button>
       </div>
-    </div>
+    </li>
   );
 };
 
