be416103a557f6bad4704ff76f531a752b7908fb
Tatiana Botia <tatianabb21@gmail.com>
2023-03-09 22:35:54 -0500

    update styles and view

--- src/view.js
+++ src/view.js
@@ -6,6 +6,7 @@ import TodoForm from "./Components/TodoForm";
 import TodoList from "./Components/TodoList";
 import TodoSearch from "./Components/TodoSearch";
 import { TodoContext } from "./TodoContext";
+import logo from "./assets/background.svg";
 
 const View = () => {
   const {
@@ -16,31 +17,35 @@ const View = () => {
     deleteTodos,
     openModal,
     setOpenModal,
-    restartTodo
+    restartTodo,
   } = useContext(TodoContext);
 
   return (
-    <>
-      <TodoCounter />
-      <TodoSearch />
-      {error && <p>Hubo un error</p>}
-      {loading && <p>Loading</p>}
-      {!loading && !searchedTodos.length > 0 && <p>Crea tu todo</p>}
-      <TodoList
-        list={searchedTodos}
-        onComplete={completeTodos}
-        onClearComplete={restartTodo}
-        onDelete={deleteTodos}
-      />
+    <section className="TodoView">
+      <div className="TodoView-img">
+        <img className="img" src={logo} alt="img-decoration" />
+      </div>
+      <div className="TodoView-todo">
+        <TodoCounter />
+        <TodoSearch />
+        {error && <p>Hubo un error</p>}
+        {loading && <p>Loading</p>}
+        {!loading && !searchedTodos.length > 0 && <p>Crea tu todo</p>}
+        <TodoList
+          list={searchedTodos}
+          onComplete={completeTodos}
+          onClearComplete={restartTodo}
+          onDelete={deleteTodos}
+        />
 
-      {openModal && (
-        <Modal>
-          <TodoForm />
-        </Modal>
-      )}
-      <CreateTodoButton openModal={openModal} setOpenModal={setOpenModal}/>
-
-    </>
+        {openModal && (
+          <Modal>
+            <TodoForm />
+          </Modal>
+        )}
+        <CreateTodoButton openModal={openModal} setOpenModal={setOpenModal} />
+      </div>
+    </section>
   );
 };
 
